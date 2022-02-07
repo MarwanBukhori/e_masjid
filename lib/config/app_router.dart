7911554f8f35bd68cd3d456f8be725d1f39323ef
marwanbukhori <marwanbukhori.dev@gmail.com>
2022-02-07 16:48:03 +0800

    base file

---
+++ lib/config/app_router.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:e_masjid/screens/screens.dart';
+
+class AppRouter{
+  static Route onGenerateRoute(RouteSettings settings){
+    print('This is route: ${settings.name}');
+
+    switch (settings.name){
+      case '/':
+        return HomeScreen.route();
+      case HomeScreen.routeName:
+        return HomeScreen.route();
+      // case CartScreen.routeName:
+      //   return CartScreen.route();
+      // case CatalogScreen.routeName:
+      //   return CatalogScreen.route();
+      // case WishlistScreen.routeName:
+      //   return WishlistScreen.route();
+      // case ProductScreen.routeName:
+      //   return ProductScreen.route();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route _errorRoute(){
+    return MaterialPageRoute(
+      settings: RouteSettings(name: '/error'),
+      builder: (_) => Scaffold(
+          appBar: AppBar(title: Text('Error'),)
+      ),
+    );
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:e_masjid/screens/screens.dart';
+
+
+import 'config/app_router.dart';
+import 'config/theme.dart';
+import 'widgets/widgets.dart';
+
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Masjid',
+      theme: theme(),
+      onGenerateRoute: AppRouter.onGenerateRoute,
+      initialRoute: HomeScreen.routeName,
+
+    );
+  }
+}
