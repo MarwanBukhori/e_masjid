48f9d49f695dc3a453be74167821db6c60764b34
marwanbukhori <marwanbukhori.dev@gmail.com>
2022-04-12 11:41:25 +0800

    done tanya imam screen design

--- lib/screens/modul_tanya/tanya_imam_screen.dart
+++ lib/screens/modul_tanya/tanya_imam_screen.dart
@@ -31,100 +31,125 @@ class _TanyaImamScreenState extends State<TanyaImamScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Center(
-            child: Padding(
-          padding: const EdgeInsets.only(right: 30.0),
-          child: Text(
-            'Tanya Imam',
+        backgroundColor: Colors.white,
+        brightness: Brightness.light,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black87,
           ),
-        )),
-        backgroundColor: kPrimaryColor,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
       ),
-      body: Container(
-        padding: EdgeInsets.all(15.0),
-        child: Column(children: [
-          SizedBox(
-            height: 5,
-          ),
-          // TextField(
-          //           //   decoration: InputDecoration(
-          //           //     prefixIcon: Icon(Icons.people),
-          //           //     border: OutlineInputBorder(),
-          //           //     labelText: 'Imam Pilihan',
-          //           //   ),
-          //           // ),
-          // SizedBox(
-          //   height: 24,
-          // ),
-          TextField(
-            controller: tajukTextController,
-            decoration: InputDecoration(
-              prefixIcon: Icon(Icons.book_rounded),
-              border: OutlineInputBorder(),
-              labelText: 'Tajuk',
+      body: ListView(
+        children: <Widget>[
+          SizedBox(height: 15.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 20 ),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+              children:<Widget>[
+                Text(
+                  'Tanya',
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 25.0,
+                      ),
+                ),
+                Text(
+                  'Imam',
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 15,
+                )
+              ]
             ),
           ),
-          SizedBox(
-            height: 24,
-          ),
-          TextField(
-            controller: huraianTextController,
-            maxLines: 4,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Huraian',
-              hintText: 'Tulis sesuatu..',
-            ),
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton.icon(
-                icon: Icon(
-                    Icons.add_box,
-                size: 27.0,),
-                style: ElevatedButton.styleFrom(
-                    primary: kPrimaryColor,
-                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                    textStyle:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                onPressed: () async {
-                  try {
-                    LoadingIndicator.showLoadingDialog(context);
+          Container(
+            padding: EdgeInsets.all(15.0),
+            child: Column(children: [
+              TextField(
+                controller: tajukTextController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.book_rounded),
+                  border: OutlineInputBorder(),
+                  labelText: 'Tajuk',
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              TextField(
+                controller: huraianTextController,
+                maxLines: 4,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Huraian',
+                  hintText: 'Tulis sesuatu..',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton.icon(
+                    icon: Icon(
+                      Icons.add_box,
+                      size: 27.0,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        primary: kPrimaryColor,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                        textStyle: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold)),
+                    onPressed: () async {
+                      try {
+                        LoadingIndicator.showLoadingDialog(context);
 
-                    final temujanji = TemuJanji(
-                        TemuJanjiID: 'TJ2',
-                        JenisTemuJanji: 'tanya',
-                        tajuk: tajukTextController.text,
-                        huraian: huraianTextController.text,
-                        tarikh: DateTime.now()
-                    );
+                        final temujanji = TemuJanji(
+                            TemuJanjiID: 'TJ2',
+                            JenisTemuJanji: 'tanya',
+                            tajuk: tajukTextController.text,
+                            huraian: huraianTextController.text,
+                            tarikh: DateTime.now());
 
-                    final result = await addTask(temujanji);
+                        final result = await addTask(temujanji);
 
-                    if (result) {
-                      Navigator.pop(context);
-                      Navigator.pop(context);
-                    } else
-                      throw 'Unable to add soalan';
-                  } catch (e) {
-                    Navigator.pop(context);
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          content: Text(e.toString()),
+                        if (result) {
+                          Navigator.pop(context);
+                          Navigator.pop(context);
+                        } else
+                          throw 'Unable to add soalan';
+                      } catch (e) {
+                        Navigator.pop(context);
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              content: Text(e.toString()),
+                            );
+                          },
                         );
-                      },
-                    );
-                  }
-                  ;
-                },
-                label: Text('Hantar Soalan'),
+                      }
+                      ;
+                    },
+                    label: Text('Hantar Soalan'),
+                  )
+                ],
               )
-            ],
-          )
-        ]),
+            ]),
+          ),
+        ],
       ),
       bottomNavigationBar: CustomNavBar(),
     );
