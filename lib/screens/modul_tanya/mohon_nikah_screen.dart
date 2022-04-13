0345572a99d1505acddda5a48a4d2f89e337e8e9
marwanbukhori <marwanbukhori.dev@gmail.com>
2022-04-13 10:25:38 +0800

    update mohon nikah & tempah qurban screen

--- lib/screens/modul_tanya/mohon_nikah_screen.dart
+++ lib/screens/modul_tanya/mohon_nikah_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:e_masjid/widgets/widgets.dart';
 import 'package:e_masjid/config/constants.dart';
 
+import '../../models/temujanji_model.dart';
+import '../../services/temujanji.service.dart';
+import '../../widgets/loading-indicator.dart';
+
 class MohonNikahScreen extends StatefulWidget {
   const MohonNikahScreen({Key? key}) : super(key: key);
   static const String routeName = '/nikah';
@@ -18,52 +22,137 @@ class MohonNikahScreen extends StatefulWidget {
 }
 
 class _MohonNikahScreenState extends State<MohonNikahScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: CustomAppBar(title: 'Mohon Nikah'),
-      body: Container(
-        padding: EdgeInsets.all(15.0),
-        child: Column(children: [
-          SizedBox(
-            height: 5,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Nama Pemohon',
+
+    final tajukTextController = new TextEditingController();
+
+    final huraianTextController = new TextEditingController();
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          brightness: Brightness.light,
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black87,
             ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
           ),
-          SizedBox(
-            height: 24,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Nama Pasangan',
+        ),
+        body: ListView(
+          children: <Widget>[
+            SizedBox(height: 15.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 20 ),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:<Widget>[
+                    Text(
+                      'Mohon',
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 25.0,
+                      ),
+                    ),
+                    Text(
+                      'Nikah',
+                      style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    )
+                  ]
+              ),
             ),
-          ),
-          SizedBox(
-            height: 24,
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: kPrimaryColor,
-                      padding:
+            Container(
+              padding: EdgeInsets.all(15.0),
+              child: Column(children: [
+                TextField(
+                  controller: tajukTextController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.edit_note_outlined),
+                    border: OutlineInputBorder(),
+                    labelText: 'Tajuk',
+                  ),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                TextField(
+                  controller: huraianTextController,
+                  maxLines: 4,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Huraian',
+                    hintText: 'Tulis sesuatu..',
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton.icon(
+                      icon: Icon(
+                        Icons.add,
+                        size: 27.0,
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: kPrimaryColor,
+                          padding:
                           EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                      textStyle:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                  onPressed: () {},
-                  child: const Text('Hantar'))
-            ],
-          )
-        ]),
-      ),
-      bottomNavigationBar: CustomNavBar(),
-    );
-  }
+                          textStyle: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold)),
+                      onPressed: () async {
+                        try {
+                          // LoadingIndicator.showLoadingDialog(context);
+                          //
+                          // final temujanji = TemuJanji(
+                          //     TemuJanjiID: 'TJ2',
+                          //     JenisTemuJanji: 'tanya',
+                          //     tajuk: tajukTextController.text,
+                          //     huraian: huraianTextController.text,
+                          //     tarikh: DateTime.now());
+                          //
+                          // final result = await addTask(temujanji);
+                          //
+                          // if (result) {
+                          //   Navigator.pop(context);
+                          //   Navigator.pop(context);
+                          // } else
+                          //   throw 'Unable to add soalan';
+                        } catch (e) {
+                          // Navigator.pop(context);
+                          // showDialog(
+                          //   context: context,
+                          //   builder: (context) {
+                          //     return AlertDialog(
+                          //       content: Text(e.toString()),
+                          //     );
+                          //   },
+                          // );
+                        }
+                        ;
+                      },
+                      label: Text('Hantar Permohonan'),
+                    )
+                  ],
+                )
+              ]),
+            ),
+          ],
+        ),
+        bottomNavigationBar: CustomNavBar(),
+      );
+    }
 }
--- lib/screens/modul_tanya/tempah_qurban_screen.dart
+++ lib/screens/modul_tanya/tempah_qurban_screen.dart
@@ -19,51 +19,136 @@ class TempahQurbanScreen extends StatefulWidget {
 }
 
 class _TempahQurbanScreenState extends State<TempahQurbanScreen> {
+
+  final tajukTextController = new TextEditingController();
+
+  final huraianTextController = new TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: CustomAppBar(title: 'Qurban'),
-      body: Container(
-        padding: EdgeInsets.all(15.0),
-        child: Column(children: [
-          SizedBox(
-            height: 5,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        brightness: Brightness.light,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black87,
           ),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Nama Pemohon',
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(height: 15.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 20 ),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children:<Widget>[
+                  Text(
+                    'Tempah',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                  Text(
+                    'Qurban',
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  )
+                ]
             ),
           ),
-          SizedBox(
-            height: 24,
+          Container(
+            padding: EdgeInsets.all(15.0),
+            child: Column(children: [
+              TextField(
+                controller: tajukTextController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.edit_note_outlined),
+                  border: OutlineInputBorder(),
+                  labelText: 'Tajuk',
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              TextField(
+                controller: huraianTextController,
+                maxLines: 4,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Huraian',
+                  hintText: 'Tulis sesuatu..',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton.icon(
+                    icon: Icon(
+                      Icons.add,
+                      size: 27.0,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        primary: kPrimaryColor,
+                        padding:
+                        EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                        textStyle: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold)),
+                    onPressed: () async {
+                      try {
+                        // LoadingIndicator.showLoadingDialog(context);
+                        //
+                        // final temujanji = TemuJanji(
+                        //     TemuJanjiID: 'TJ2',
+                        //     JenisTemuJanji: 'tanya',
+                        //     tajuk: tajukTextController.text,
+                        //     huraian: huraianTextController.text,
+                        //     tarikh: DateTime.now());
+                        //
+                        // final result = await addTask(temujanji);
+                        //
+                        // if (result) {
+                        //   Navigator.pop(context);
+                        //   Navigator.pop(context);
+                        // } else
+                        //   throw 'Unable to add soalan';
+                      } catch (e) {
+                        // Navigator.pop(context);
+                        // showDialog(
+                        //   context: context,
+                        //   builder: (context) {
+                        //     return AlertDialog(
+                        //       content: Text(e.toString()),
+                        //     );
+                        //   },
+                        // );
+                      }
+                      ;
+                    },
+                    label: Text('Hantar Permohonan'),
+                  )
+                ],
+              )
+            ]),
           ),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Kuantiti Slot/Bahagian',
-            ),
-          ),
-          SizedBox(
-            height: 24,
-          ),
-
-          ButtonBar(
-            alignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: kPrimaryColor,
-                      padding:
-                      EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                      textStyle:
-                      TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                  onPressed: () {},
-                  child: const Text('Tempah'))
-            ],
-          )
-        ]),
+        ],
       ),
       bottomNavigationBar: CustomNavBar(),
     );
